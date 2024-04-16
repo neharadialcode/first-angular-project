647c4e42d06b3d45aaf30fdce14f2dbbb3fbaec2
Neha <neharadialcode@gmail.com>
2024-04-16 12:48:18 +0530

    singe component and muliple components in a folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, HomeComponent],
+  imports: [RouterOutlet, MatButtonModule, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
