d07ed131725a147208748a4424a7a89c69592383
Neha <neharadialcode@gmail.com>
2024-04-16 12:15:32 +0530

    component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
