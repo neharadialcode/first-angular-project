c34f7e8b5d24f87b52c3dda4cf27e03d09ea906d
Neha <neharadialcode@gmail.com>
2024-04-16 15:07:25 +0530

    component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, HomeComponent, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    HomeComponent,
+    HeaderComponent,
+    ProductCardComponent,
+    MatCardModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
